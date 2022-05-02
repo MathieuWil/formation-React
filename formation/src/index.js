b4faae2e1fa300f8668f6c98678a616ca295cbbd
MathieuWil <mathieu0108w@gmail.com>
2022-05-02 16:10:18 +0200

    Exercices

--- formation/src/index.js
+++ formation/src/index.js
@@ -1,5 +1,23 @@
-console.log('Application react')
+import {
+  bonjour,
+  bonjour2,
+  calculateAverage,
+  displayNotes,
+  displayStudentName,
+  student,
+} from './exos/revision'
 
-if (true) {
-  console.log('waw')
-}
+// Exercice 1
+console.log(bonjour('Mathieu'))
+
+// Exercice 2
+console.log(bonjour2('Mathieu2'))
+
+// Exercice 3
+console.log(displayStudentName(student))
+
+// Exercice 4
+console.log(displayNotes())
+
+// Exercice 5
+console.log(calculateAverage())
