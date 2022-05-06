82b068bf68b0c018d8f6467150822491a3f93925
MathieuWil <mathieu0108w@gmail.com>
2022-05-06 14:27:35 +0200

    aprem_vendredi

--- formation/src/index.js
+++ formation/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { render } from 'react-dom'
-import './index.css'
-import App from './exos/carnet-notes/App'
-// import App from './exos/conteur/App'
-// import { render } from 'react-dom'
-// // import App from './example/AppEvent'
-// // import App from './exos/design-styled-component/App'
-// import App from './exos/todo/App'
 
-render(<App />, document.querySelector('#root'))
+import { createRoot } from 'react-dom/client'
+
+import App from './todo-app/App'
+
+const root = createRoot(document.querySelector('#root'))
+
+root.render(<App />)
